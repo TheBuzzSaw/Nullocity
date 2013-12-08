787c64bce0969758dd6d5097f27a5932892f0e99
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2013-12-08 15:25:18 -0700

    Moved builders to new home.

---
+++ source/Builders.hpp
@@ -0,0 +1,10 @@
+#ifndef BUILDERS_HPP
+#define BUILDERS_HPP
+
+#include "SimpleBufferObject.hpp"
+
+SimpleBufferObject BuildCube();
+SimpleBufferObject BuildPyramid();
+SimpleBufferObject BuildLines();
+
+#endif
--- source/GameModule.cpp
+++ source/GameModule.cpp
@@ -1,122 +1,14 @@
 #include "GameModule.hpp"
+#include "Builders.hpp"
 #include <SDL2TK/Matrix4x4.hpp>
-#include <cmath>
 using namespace std;
 using namespace SDL2TK;
-
-static const Vector4F Black(0.0f, 0.0f, 0.0f, 1.0f);
-static const Vector4F Red(1.0f, 0.0f, 0.0f, 1.0f);
-static const Vector4F Green(0.0f, 1.0f, 0.0f, 1.0f);
-static const Vector4F Blue(0.0f, 0.0f, 1.0f, 1.0f);
-static const Vector4F Yellow(1.0f, 1.0f, 0.0f, 1.0f);
-static const Vector4F Magenta(1.0f, 0.0f, 1.0f, 1.0f);
-static const Vector4F Cyan(0.0f, 1.0f, 1.0f, 1.0f);
-
-static SimpleBufferObject BuildCube()
-{
-    const float N = 1.0f;
-
-    SimpleBuilder builder;
-    builder.Reserve(36);
-
-    builder.Add(Vector3F(N, N, N), Red);
-    builder.Add(Vector3F(N, N, -N), Red);
-    builder.Add(Vector3F(N, -N, -N), Red);
-
-    builder.Add(Vector3F(N, N, N), Red);
-    builder.Add(Vector3F(N, -N, -N), Red);
-    builder.Add(Vector3F(N, -N, N), Red);
-
-    builder.Add(Vector3F(N, N, N), Green);
-    builder.Add(Vector3F(N, -N, N), Green);
-    builder.Add(Vector3F(-N, -N, N), Green);
-
-    builder.Add(Vector3F(N, N, N), Green);
-    builder.Add(Vector3F(-N, -N, N), Green);
-    builder.Add(Vector3F(-N, N, N), Green);
-
-    builder.Add(Vector3F(N, N, N), Blue);
-    builder.Add(Vector3F(-N, N, N), Blue);
-    builder.Add(Vector3F(-N, N, -N), Blue);
-
-    builder.Add(Vector3F(N, N, N), Blue);
-    builder.Add(Vector3F(-N, N, -N), Blue);
-    builder.Add(Vector3F(N, N, -N), Blue);
-
-    builder.Add(Vector3F(-N, -N, -N), Cyan);
-    builder.Add(Vector3F(-N, N, -N), Cyan);
-    builder.Add(Vector3F(-N, N, N), Cyan);
-
-    builder.Add(Vector3F(-N, -N, -N), Cyan);
-    builder.Add(Vector3F(-N, N, N), Cyan);
-    builder.Add(Vector3F(-N, -N, N), Cyan);
-
-    builder.Add(Vector3F(-N, -N, -N), Magenta);
-    builder.Add(Vector3F(N, -N, -N), Magenta);
-    builder.Add(Vector3F(N, N, -N), Magenta);
-
-    builder.Add(Vector3F(-N, -N, -N), Magenta);
-    builder.Add(Vector3F(N, N, -N), Magenta);
-    builder.Add(Vector3F(-N, N, -N), Magenta);
-
-    builder.Add(Vector3F(-N, -N, -N), Yellow);
-    builder.Add(Vector3F(-N, -N, N), Yellow);
-    builder.Add(Vector3F(N, -N, N), Yellow);
-
-    builder.Add(Vector3F(-N, -N, -N), Yellow);
-    builder.Add(Vector3F(N, -N, N), Yellow);
-    builder.Add(Vector3F(N, -N, -N), Yellow);
-
-    return SimpleBufferObject(builder);
-}
-
-static SimpleBufferObject BuildPyramid()
-{
-    const float RootTwo = 1.414213562f;
-    const float Z = 1.0f / RootTwo;
-
-    Vector3F positions[4];
-
-    positions[0] = Vector3F(1.0f, 0.0f, -Z);
-    positions[1] = Vector3F(-1.0f, 0.0f, -Z);
-    positions[2] = Vector3F(0.0f, 1.0f, Z);
-    positions[3] = Vector3F(0.0f, -1.0f, Z);
-
-    SimpleBuilder builder;
-    builder.Reserve(12);
-
-    builder.Add(positions[0], Red);
-    builder.Add(positions[2], Red);
-    builder.Add(positions[1], Red);
-
-    builder.Add(positions[0], Green);
-    builder.Add(positions[1], Green);
-    builder.Add(positions[3], Green);
-
-    builder.Add(positions[1], Blue);
-    builder.Add(positions[2], Blue);
-    builder.Add(positions[3], Blue);
-
-    builder.Add(positions[0], Yellow);
-    builder.Add(positions[3], Yellow);
-    builder.Add(positions[2], Yellow);
-
-    return SimpleBufferObject(builder);
-}
 
 GameModule::GameModule()
 {
     (void)BuildPyramid;
     _cubeObject = BuildCube();
-
-    SimpleBuilder builder;
-
-    builder.Add(Vector3F(-7.0f, 0.0f, 0.0f), Black);
-    builder.Add(Vector3F(7.0f, 0.0f, 0.0f), Black);
-    builder.Add(Vector3F(0.0f, -7.0f, 0.0f), Black);
-    builder.Add(Vector3F(0.0f, 7.0f, 0.0f), Black);
-
-    _linesObject = SimpleBufferObject(builder);
+    _linesObject = BuildLines();
 
     _camera.Distance(32.0f);
     _camera.Vertical(RotationF::FromDegrees(-45.0f));
