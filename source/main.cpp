7b8616bb6786838af700c3aa920e59d8c069b873
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2014-04-02 23:41:36 -0600

    Fixed linking in Linux.

--- source/main.cpp
+++ source/main.cpp
@@ -1,18 +1,8 @@
 #include <SDL2TK/Window.hpp>
 #include <SDL_image.h>
 #include "GameModule.hpp"
-#include "LuaState.hpp"
 #include <iostream>
 using namespace std;
-
-void TestLua()
-{
-    LuaState lua;
-    cout << "LUA\n";
-    lua.Execute("io.write(777)");
-    //lua.Execute("asdf();");
-    lua.Execute("Nullocity.Test()");
-}
 
 void Start()
 {
@@ -28,14 +18,12 @@ void Start()
 
 int main(int argc, char** argv)
 {
-    TestLua();
-
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
-    //IMG_Init(IMG_INIT_PNG);
+    IMG_Init(IMG_INIT_PNG);
 
     Start();
 
-    //IMG_Quit();
+    IMG_Quit();
     SDL_Quit();
 
     return 0;
