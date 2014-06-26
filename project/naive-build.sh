061a3bafe82882478f82d1e6097da15749af436e
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2014-06-26 13:22:38 -0600

    Added optimization flags.

--- project/naive-build.sh
+++ project/naive-build.sh
@@ -1,2 +1,2 @@
-clang -c ../../SDL2TK/source/glew.c
-clang++ -std=c++11 -framework OpenGL -framework OpenAL -framework SDL2 -I/Library/Frameworks/SDL2.framework/Headers -I../../SDL2TK/include ../../SDL2TK/source/*.cpp ../source/*.cpp glew.o -llua -o game.bin
+clang -O3 -c ../../SDL2TK/source/glew.c
+clang++ -std=c++11 -O3 -framework OpenGL -framework OpenAL -framework SDL2 -I/Library/Frameworks/SDL2.framework/Headers -I../../SDL2TK/include ../../SDL2TK/source/*.cpp ../source/*.cpp glew.o -llua -o game.bin
