70e3ae4b925f269a78b0c2a208d59c88bf9430e8
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2015-08-09 22:37:00 -0600

    Split out glew from build steps in OS X.

--- project/build-osx.sh
+++ project/build-osx.sh
@@ -1,1 +1,2 @@
-clang++ -std=c++11 -O2 -o Nullocity.bin -I/usr/local/include -I/usr/local/include/SDL2 -I../SDL2TK/include ../source/*.cpp ../SDL2TK/source/*.cpp ../SDL2TK/source/glew.c -L/usr/local/lib -lSDL2 -lSDL2_image -llua -framework OpenGL -framework OpenAL
+clang -O2 -c ../SDL2TK/source/glew.c
+clang++ -std=c++11 -O2 -o Nullocity.bin -I/usr/local/include -I/usr/local/include/SDL2 -I../SDL2TK/include ../source/*.cpp ../SDL2TK/source/*.cpp ./glew.o -L/usr/local/lib -lSDL2 -lSDL2_image -llua -framework OpenGL -framework OpenAL
